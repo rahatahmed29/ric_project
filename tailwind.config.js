5ee3505c0e552bfd18d6801482b26b199ee94423
Rahat <rahatofficial.29@gmail.com>
2025-06-18 02:34:25 +0600

    Clean initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"], // 👈 Tells Tailwind where to look
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
